4fe1049dc63e318c102ba98e1e72d8b0f57f80fb
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-28 17:16:44 +0330

    feat: initial commit for frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dữ Liệu Thực Tế</h1>
+      <p>Thông tin về các xu hướng xã hội và kinh tế tại Việt Nam.</p>
+    </div>
+  );
+}
+
+export default App;
