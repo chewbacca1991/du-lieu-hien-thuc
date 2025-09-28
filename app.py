90489f8937c22e45e9763e719d2c30a6652679f0
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-28 17:17:34 +0330

    Updated response message for clarity.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    return jsonify({'message': 'Data retrieved successfully!'})
+    return jsonify({'message': 'Data has been successfully retrieved!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
